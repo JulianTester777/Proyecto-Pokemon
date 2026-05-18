bacdc169f55ac1bbb5ea4d913913130ae82b0bac
Amerika <amerikaegg@gmail.com>
2026-05-18 13:48:19 -0500

    entrenador.ex
    Nuevos campos para autenticación y equipo activo

--- lib/pokemon_battle/entrenador.ex
+++ lib/pokemon_battle/entrenador.ex
@@ -2,11 +2,13 @@ defmodule PokemonBattle.Entrenador do
   @enforce_keys [:nombre]
   defstruct [
     :nombre,
+    :clave,
     monedas: 0,
     monedas_acumuladas: 0,
     victorias: 0,
     coleccion: [],
     sobres_pendientes: [],
-    equipos: []
+    equipos: [],
+    equipo_actual: nil
   ]
 end
